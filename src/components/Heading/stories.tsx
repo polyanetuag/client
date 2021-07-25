5ef651b09a599b18ba633ab3d8599b0f3f35ed2b
Polyane Tuag <pollyportugues@hotmail.com>
2021-07-25 17:36:47 -0300

    Creating heading component

---
+++ src/components/Heading/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import Heading from '.'
+
+export default {
+  title: 'Heading',
+  component: Heading
+} as Meta
+
+export const Default: Story = () => <Heading />
