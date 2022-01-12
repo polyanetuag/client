5e50d50a42f05b9466b9d2ffe7436c42773060a2
Polyane Tuag <pollyportugues@hotmail.com>
2022-01-12 18:00:44 -0300

    fix: modifying font size - test

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -35,4 +35,10 @@ describe('<Heading />', () => {
       }
     )
   })
+  it('should render a heading with a small size', () => {
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.6rem'
+    })
+  })
 })
