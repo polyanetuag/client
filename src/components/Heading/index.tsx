961e9806debc8f7e6706801de79091abf9ab2b89
Polyane Tuag <pollyportugues@hotmail.com>
2022-01-13 16:00:18 -0300

    fix: exporting type LineColors

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,10 +1,13 @@
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
   size?: 'small' | 'medium'
 }
 
@@ -13,12 +16,14 @@ const Heading = ({
   color = 'white',
   lineLeft = false,
   lineBottom = false,
+  lineColor = 'primary',
   size = 'medium'
 }: HeadingProps) => (
   <S.Wrapper
     color={color}
     lineLeft={lineLeft}
     lineBottom={lineBottom}
+    lineColor={lineColor}
     size={size}
   >
     {children}
