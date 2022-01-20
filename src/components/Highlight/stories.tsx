398931792d6bade4995f0a4aa6137fb6b6c3439b
Polyane Tuag <pollyportugues@hotmail.com>
2022-01-20 19:07:52 -0300

    feat: adding backgroundImage

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -7,6 +7,7 @@ export default {
   args: {
     title: 'Read Dead it’s back',
     subtitle: 'Come see John’s new adventures',
+    backgroundImage: '/img/red-dead-img.jpeg',
     buttonLabel: 'Buy now',
     buttonLink: '/rdr2'
   }
