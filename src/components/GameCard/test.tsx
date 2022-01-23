a15035a573318f9ccecae8ab2ddc9b2c2e642ee8
Polyane Tuag <pollyportugues@hotmail.com>
2022-01-23 06:12:51 -0300

    feat: Gamecard tests

---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,38 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import GameCard from '.'
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00'
+}
+describe('<GameCard />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<GameCard {...props} />)
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    // expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
+  })
+
+  it('should render price in label', () => {
+    // renderiza o componente
+    // preço não tenha line-through
+    // preço tenha o background secundário
+  })
+
+  it('should render a line-through in price when promotional', () => {
+    // renderiza o componente (COM promotionalPrice) // 200 reais // 15 reais
+    // preço tenha line-through (200)
+    // preço novo promocional não vai ter line-through (15)
+  })
+})
