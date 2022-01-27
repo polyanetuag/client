a0a0e3cc4f409931bbaa08c759bd6ac4ae58a148
Polyane Tuag <pollyportugues@hotmail.com>
2022-01-27 14:24:43 -0300

    feat: adding ribbon to gamecard - stories

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -9,15 +9,11 @@ export default {
     developer: 'Rockstar Games',
     img: 'https://source.unsplash.com/user/willianjusten/300x140',
     price: 'R$ 235,00',
-    promotionalPrice: 'R$ 200,00'
+    promotionalPrice: 'R$ 215,00'
   },
   argTypes: {
-    onFav: { action: 'clicked' }
-  },
-  parameters: {
-    backgrounds: {
-      default: 'dark'
-    }
+    onFav: { action: 'clicked' },
+    ribbon: { type: 'string' }
   }
 } as Meta
 
@@ -26,3 +22,15 @@ export const Default: Story<GameCardProps> = (args) => (
     <GameCard {...args} />
   </div>
 )
+
+export const WithRibbon: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'small',
+  ribbonColor: 'primary'
+}
