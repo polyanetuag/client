e936a246d2426e57644836ffd455e6c3992c50ea
Polyane Tuag <pollyportugues@hotmail.com>
2022-02-09 19:44:45 -0300

    feat: implementing tests

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render the heading', () => {
+    render(<Checkbox />)
+  })
+})
