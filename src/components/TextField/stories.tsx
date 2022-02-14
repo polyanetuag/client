38f5153568fb67948fe68fefb6069ba409e68132
Polyane Tuag <pollyportugues@hotmail.com>
2022-02-14 19:43:09 -0300

    feat: adding icon stories in TextField

--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -1,5 +1,6 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import TextField, { TextFieldProps } from '.'
+import { Email } from '@styled-icons/material-outlined'
 
 export default {
   title: 'TextField',
@@ -7,12 +8,14 @@ export default {
   args: {
     label: 'E-mail',
     labelFor: 'Email',
+    icon: <Email />,
     id: 'Email',
     initialValue: '',
     placeholder: 'john.cage@gmail.com'
   },
   argTypes: {
-    onInput: { action: 'changed' }
+    onInput: { action: 'changed' },
+    icon: { type: '' }
   }
 } as Meta
 
