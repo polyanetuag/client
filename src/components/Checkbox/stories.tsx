a207985a8bd818a7143c09843e56c937f197cdc4
Polyane Tuag <pollyportugues@hotmail.com>
2022-02-18 18:36:34 -0300

    fix: organizing storybook

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -2,7 +2,7 @@ import { Story, Meta } from '@storybook/react'
 import Checkbox, { CheckboxProps } from '.'
 
 export default {
-  title: 'Checkbox',
+  title: 'Form/Checkbox',
   component: Checkbox,
   parameters: {
     layout: 'fullscreen',
