7d9e2fe21d18a77eab6ae94934e3781e7c0b8137
Polyane Tuag <pollyportugues@hotmail.com>
2022-02-23 13:21:07 -0300

    feat: adding new huge size to Auth component

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -13,7 +13,7 @@ const Auth = ({ title, children }: AuthProps) => (
       <S.BannerContent>
         <Logo id="banner" />
         <div>
-          <Heading>All your favorite games in one place</Heading>
+          <Heading size="huge">All your favorite games in one place</Heading>
           <S.Subtitle>
             <strong>WON</strong> is the best and most complete gaming platform
           </S.Subtitle>
