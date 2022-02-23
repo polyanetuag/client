e3742670b528832d9df3f46edb5b0eea77b1dbbf
Polyane Tuag <pollyportugues@hotmail.com>
2022-02-23 13:18:53 -0300

    feat: huge size tests in heading

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -35,6 +35,15 @@ describe('<Heading />', () => {
       }
     )
   })
+
+  it('should render a heading with a huge size', () => {
+    renderWithTheme(<Heading size="huge">Won Games</Heading>)
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '5.2rem'
+    })
+  })
+
   it('should render a heading with a small size', () => {
     renderWithTheme(<Heading size="small">Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
