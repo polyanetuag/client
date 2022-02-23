78796926aac51436e76b67f3aaf590bd02ff386a
Polyane Tuag <pollyportugues@hotmail.com>
2022-02-23 13:18:07 -0300

    feat: huge size styles in heading

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -18,6 +18,9 @@ const wrapperModifiers = {
     font-size: ${theme.font.sizes.xxlarge};
 `}
   `,
+  huge: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.huge};
+  `,
 
   lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
     padding-left: ${theme.spacings.xsmall};
